b44b8f67732c4a5222351f839380a7732c39404a
vitekkuz <vitekkuz@mail.ru>
2022-07-18 19:38:00 +0300

    ебольшие изменения по выводу справочников

--- task_app/views.py
+++ task_app/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
 
-from .models import User
+from .models import User, Aria, District, City, PhoneNumber, Profession
 
 # Create your views here.
 
 def index(request):
 
     users = User.objects.all()
-    context = {'users': users}
+    professions = Profession.objects.all()
+    phoneNumbers = PhoneNumber.objects.all()
+    cities = City.objects.all()
+    arias = Aria.objects.all()
+    districts = District.objects.all()
+    context = {'users': users,
+               'professions': professions,
+               'phoneNumbers': phoneNumbers,
+               'cities': cities,
+               'arias': arias,
+               'districts': districts,
+               }
 
 
     return render(request, 'task_app/base.html', context=context)
